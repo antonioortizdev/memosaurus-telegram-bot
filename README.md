ce0dbb512d91a8998af0392cff4bed97a03ca7f3
Antonio Ortiz <antonioortizdev@gmail.com>
2023-05-01 13:09:38 +0200

    make bot reply texts with audios

---
+++ README.md
@@ -0,0 +1,23 @@
+# Memosaurus Telegram Bot
+It's a simple Telegram bot that allows you to create reminders that will be replied to you by voice.
+
+## Requirements
+- Telegram Bot Token
+- Python 3.11+
+- Pip 23.1+
+- Google Cloud CLI 428.0+
+- Google Cloud Text-to-Speech API enabled
+
+## Installation
+```bash
+git clone https://github.com/antonioortizdev/memosaurus-telegram-bot
+cd memosaurus-telegram-bot
+python -m virtualenv memosaurus-telegram-bot
+source memosaurus-telegram-bot/bin/activate
+pip install -r requirements.txt
+```
+
+## Usage
+```bash
+python src/main.py
+```
--- src/text_to_speech.py
+++ src/text_to_speech.py
@@ -1,39 +1,52 @@
 import configuration
+import os
 from time import time_ns
 from logs import log
 
 
-def synthesize_text(text) -> None:
-    """Synthesizes speech from the input string of text."""
-    from google.cloud import texttospeech
+def synthesize_text(text) -> str:
+	"""Synthesizes speech from the input string of text."""
+	from google.cloud import texttospeech
 
-    client = texttospeech.TextToSpeechClient()
+	client = texttospeech.TextToSpeechClient()
 
-    input_text = texttospeech.SynthesisInput(text=text)
+	input_text = texttospeech.SynthesisInput(text=text)
 
-    # Note: the voice can also be specified by name.
-    # Names of voices can be retrieved with client.list_voices().
-    voice = texttospeech.VoiceSelectionParams(
-        language_code=configuration.get("TTS_LANGUAGE_CODE"),
-        name=configuration.get("TTS_VOICE_NAME"),
-        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
-    )
+	# Note: the voice can also be specified by name.
+	# Names of voices can be retrieved with client.list_voices().
+	voice = texttospeech.VoiceSelectionParams(
+		language_code=configuration.get("GOOGLE_TTS_LANGUAGE_CODE"),
+		name=configuration.get("GOOGLE_TTS_VOICE_NAME"),
+		ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
+	)
 
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.MP3
-    )
+	audio_config = texttospeech.AudioConfig(
+		audio_encoding=texttospeech.AudioEncoding.MP3
+	)
 
-    response = client.synthesize_speech(
-        request={"input": input_text, "voice": voice,
-                 "audio_config": audio_config}
-    )
+	response = client.synthesize_speech(
+		request={"input": input_text, "voice": voice,
+				 "audio_config": audio_config}
+	)
 
-    # The response's audio_content is binary.
-    now = time_ns()
-    with open(f'{now}.mp3', "wb") as out:
-        out.write(response.audio_content)
-        log(f'Audio content written to file "{now}.mp3"')
+	voices_dir = configuration.get('VOICES_DIR')
+	if not os.path.exists(voices_dir):
+		os.makedirs(voices_dir)
+
+	# The response's audio_content is binary.
+	now = time_ns()
+	voice_path = f'{voices_dir}{now}.mp3'
+	with open(voice_path, "wb") as out:
+		out.write(response.audio_content)
+		log(f'Audio content written to file "{voice_path}"')
+		return voice_path
 
 
-def text_to_speech(text: str):
-    return synthesize_text(text)
+def text_to_speech(text: str) -> str:
+	"""
+		Converts text to speech and returns the path to the audio file.
+
+		:param text: The text to be converted to speech.
+		:return: The path to the audio file.
+	"""
+	return synthesize_text(text)
